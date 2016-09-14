6fdbb7a7ea61e342ced6db5aff1bda9e350ea543
Kameisler <kameisler@gmail.com>
2016-09-14 13:27:43 +0200

    Ryddet JS fil internetPage.js

--- public/JS/internetPage.js
+++ public/JS/internetPage.js
@@ -14,50 +14,12 @@ var xhttp=new XMLHttpRequest();
      }
   });
 });
-document.addEventListener('DOMContentLoaded', function() {
-   clock();
-    document.getElementById('mainContent').scrolling='no';
-    document.getElementById('mainContent').frameBorder='0';
-    document.getElementById('mainContent').width='650px';
 
-}, false);
 // function to switch between color palettes in right side menu
-function changeColor(id){
-if(id==1) {
-    document.getElementById('body').style.background='#b4dbc0'
-    document.getElementById('sect1').style.background='#57bc90'
-    document.getElementById('sect2').style.background='#57bc90'
-    document.getElementById('nav').style.background='#57bc90'
-    document.getElementById('footer').style.background='#57bc90'
-}else if(id==2){
-    document.getElementById('body').style.background='#caebf2'
-    document.getElementById('sect1').style.background='#77c9d4'
-    document.getElementById('sect2').style.background='#77c9d4'
-    document.getElementById('nav').style.background='#77c9d4'
-    document.getElementById('footer').style.background='#77c9d4'
-}else if(id==3) {
 
-    document.getElementById('body').style.background = '#EEF1F3'
-    document.getElementById('sect1').style.background='#D5D5D5'
-    document.getElementById('sect2').style.background='#D5D5D5'
-    document.getElementById('nav').style.background='#D5D5D5'
-    document.getElementById('footer').style.background='#D5D5D5'
-}
-};
 
-//Sets the iframe content/handles navigation between partials
-function setMainContent(link){
-    document.getElementById('mainContent').src=link;
-  document.getElementById('mainContent').style.height='400px';
 
-};
-//updtes clock every 1000 milliseconds
-function clock(){
-var currentTime=time();
-    document.getElementById('currentTime').innerHTML=currentTime;
-    setTimeout(clock, 1000);
-};
-//Gets the current time for clock
+
 function time (){
     var now= new Date();
     var h=now.getHours();
@@ -74,6 +36,7 @@ function checkBrowser(){
     return isChrome;
 };
 //Pirate virus right side menu
+/*
 function pirate(){
     var chrome=checkBrowser();
     console.log(chrome);
@@ -92,6 +55,7 @@ function pirate(){
 
 
 }
+*/
 //resizes the iframe for the starting page.
 function resizeIframe(obj) {
     obj.style.height = obj.contentWindow.document.body.scrollHeight+20 + 'px';
