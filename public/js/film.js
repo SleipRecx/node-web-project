f77cb98562721bad0105adccb2249cfd83535ef4
Stian Dysthe <dysthe_stian@hotmail.com>
2016-09-15 13:02:24 +0200

    Added imdb plugin to film.js

--- public/js/film.js
+++ public/js/film.js
@@ -1,7 +1,6 @@
-
 var imageIndex = 1;
 
-window.onload = function (){
+window.onload = function () {
     showImage(imageIndex);
 };
 
@@ -18,8 +17,12 @@ function showImage(n) {
     var current_image_list = document.getElementsByClassName("current-image");
     var current_image_text_list = document.getElementsByClassName("current-image-text");
     var images = document.getElementsByClassName("image");
-    if (n > current_image_list.length) {imageIndex = 1}
-    if (n < 1) {imageIndex = current_image_list.length}
+    if (n > current_image_list.length) {
+        imageIndex = 1
+    }
+    if (n < 1) {
+        imageIndex = current_image_list.length
+    }
     for (i = 0; i < current_image_list.length; i++) {
         current_image_list[i].style.display = "none";
         current_image_text_list[i].style.display = "none";
@@ -27,7 +30,20 @@ function showImage(n) {
     for (i = 0; i < images.length; i++) {
         images[i].className = images[i].className.replace(" chosen-image", "");
     }
-    current_image_list[imageIndex-1].style.display = "inline";
-    current_image_text_list[imageIndex-1].style.display = "inline";
-    images[imageIndex-1].className += " chosen-image";
+    current_image_list[imageIndex - 1].style.display = "inline";
+    current_image_text_list[imageIndex - 1].style.display = "inline";
+    images[imageIndex - 1].className += " chosen-image";
 }
+
+/* IMDB Plugin */
+
+(function (d, s, id) {
+    var js, stags = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "http://g-ec2.images-amazon.com/images/G/01/imdb/plugins/rating/js/rating.min.js";
+    stags.parentNode.insertBefore(js, stags);
+})(document, 'script', 'imdb-rating-api');
